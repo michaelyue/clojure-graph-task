fbf8202dd2615e400e9d1f8ee89eef0fe9c9c749
Michael <463329348@qq.com>
2024-10-06 22:11:13 +1100

    init added

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to myclj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/myclj/core_test.clj
@@ -0,0 +1,7 @@
+(ns myclj.core-test
+  (:require [clojure.test :refer :all]
+            [myclj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
